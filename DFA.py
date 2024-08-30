2e525ba8de76dbc222f4858df95a7276542480e0
TanmayRaut058 <2022bit058@sggs.ac.in>
2024-08-30 22:30:28 +0530

    Added DFA program

--- DFA.py
+++ DFA.py
@@ -1,19 +1,33 @@
-alphabet = {'a','b'}
+alphabet = {'a', 'b'}
 
 def DFA_string_ends_with_a(text):
-	final_states = {'q1'}
-	state = q0
-	if state in final_states:
-		return "Accepted"
-	else:
-		return "Rejected"
+    final_states = {'q1'} 
+    state = q0(text) 
+    if state in final_states:
+        return "Accepted"
+    else:
+        return "Rejected"
 
 def q0(text):
-	if (text[0] in alphabet):
-		if (text == ""):
-			return "Accepted"
-		elif (text[0] == 'a'):
-			p=1
-		else :
-			return "Rejected"
-	
+    if not text: 
+        return 'q0'
+    
+    if text[0] == 'a':
+        return q1(text[1:])  
+    elif text[0] == 'b':
+        return q0(text[1:]) 
+    else:
+        return 'q0'  
+
+def q1(text):
+    if not text:  
+        return 'q1'
+    
+    if text[0] == 'a':
+        return q1(text[1:])  
+    elif text[0] == 'b':
+        return q0(text[1:])  
+    else:
+        return 'q0' 
+
+print(DFA_string_ends_with_a("b"))  
