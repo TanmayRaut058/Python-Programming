ce65a914fcd84a625aea0a21140f1d67d370b6ff
TanmayRaut058 <2022bit058@sggs.ac.in>
2024-10-01 07:57:59 +0530

    added odd even count

---
+++ odd_even_count.py
@@ -0,0 +1,10 @@
+def get_odd_even_count(l):
+	even_count = 0; odd_count = 0
+	for n in l:
+		even_count += n%2
+		odd_count += n%2
+
+	return odd_count, even_count
+
+l = [1,2,3,4,5,6,7,8,9]
+print(get_odd_even_count(l))
